57551a4d50284c462d2cd997dc772ecb307955cc
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-09-08 20:56:55 +1000

    Enum for bitwarden data types and for each TOTP type

---
+++ app/src/main/java/dev/mcarr/totpconverter/enums/BitwardenType.kt
@@ -0,0 +1,8 @@
+package dev.mcarr.totpconverter.enums
+
+enum class BitwardenType(value: Int){
+    LOGIN(1),
+    SECURE_NOTE(2),
+    CARD(3),
+    IDENTITY(4)
+}
---
+++ app/src/main/java/dev/mcarr/totpconverter/enums/FormatNames.kt
@@ -0,0 +1,8 @@
+package dev.mcarr.totpconverter.enums
+
+enum class FormatNames(value: String){
+    BITWARDEN("bitwarden"),
+    TWOFAUTH("2fauth"),
+    AEGIS("aegis"),
+    PROTON("proton")
+}
