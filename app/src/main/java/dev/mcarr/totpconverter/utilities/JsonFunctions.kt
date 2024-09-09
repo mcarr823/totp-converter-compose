7bad93b8c9bc3fd0ecad609a3c60d5eef95ee102
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-09-09 20:23:44 +1000

    Updated JSON parsing config to allow for optional fields

--- app/src/main/java/dev/mcarr/totpconverter/utilities/JsonFunctions.kt
+++ app/src/main/java/dev/mcarr/totpconverter/utilities/JsonFunctions.kt
@@ -3,6 +3,9 @@ package dev.mcarr.totpconverter.utilities
 import kotlinx.serialization.json.Json
 
 inline fun <reified T> jsonDecode(str: String): T {
-    val format = Json{ ignoreUnknownKeys=true }
+    val format = Json {
+        ignoreUnknownKeys = true
+        explicitNulls = false
+    }
     return format.decodeFromString<T>(str)
 }
