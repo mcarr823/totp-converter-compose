2ce71d441cf0a8d59368048db8e81a421db7bdff
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-09-11 18:14:31 +1000

    2fauth now uses instant instead of string for datetime attributes

--- app/src/main/java/dev/mcarr/totpconverter/classes/twofauth/TwoFAuthImportJson.kt
+++ app/src/main/java/dev/mcarr/totpconverter/classes/twofauth/TwoFAuthImportJson.kt
@@ -1,12 +1,13 @@
 package dev.mcarr.totpconverter.classes.twofauth
 
 import dev.mcarr.totpconverter.interfaces.twofauth.ITwoFAuthImportJson
+import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TwoFAuthImportJson(
     override val app: String,
     override val schema: Int,
-    override val datetime: String,
+    override val datetime: Instant,
     override val data: List<TwoFAuthImportToken>
 ) : ITwoFAuthImportJson
--- app/src/main/java/dev/mcarr/totpconverter/interfaces/twofauth/ITwoFAuthImportJson.kt
+++ app/src/main/java/dev/mcarr/totpconverter/interfaces/twofauth/ITwoFAuthImportJson.kt
@@ -1,9 +1,11 @@
 package dev.mcarr.totpconverter.interfaces.twofauth
 
+import kotlinx.datetime.Instant
+
 interface ITwoFAuthImportJson{
-    val app: String; //eg. 2fauth_v5.1.1
-    val schema: Int;
-    val datetime: String; //eg. 2024-05-14T08:57:35.542766Z
-    val data: List<ITwoFAuthImportToken>;
+    val app: String //eg. 2fauth_v5.1.1
+    val schema: Int
+    val datetime: Instant //eg. 2024-05-14T08:57:35.542766Z
+    val data: List<ITwoFAuthImportToken>
 }
 
