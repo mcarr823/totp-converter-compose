e51d6a99f69624683b471f987a1ba9d04f6c6ac8
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-09-12 18:31:52 +1000

    Split bitwarden test data class into separate functions

--- app/src/test/java/dev/mcarr/totpconverter/classes/bitwarden/BitwardenJsonTest.kt
+++ app/src/test/java/dev/mcarr/totpconverter/classes/bitwarden/BitwardenJsonTest.kt
@@ -1,5 +1,6 @@
 package dev.mcarr.totpconverter.classes.bitwarden
 
+import dev.mcarr.totpconverter.classes.GenericJsonEntry
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,7 +11,7 @@ class BitwardenJsonTest{
 
     @Test
     fun testImportExport(){
-        val testData = BitwardenTestData()
+        val testData = BitwardenTestData.getFullObject()
         val bw = BitwardenJson.import(testData)
 
         // Only 2 of the 5 items are actually 2FA tokens (type === 1),
@@ -20,20 +21,35 @@ class BitwardenJsonTest{
         // Check entry 1
         // Note that the name="x" field is checked against
         // the issuer instead, since the issuer wasn't defined.
-        val token = bw.entries[0]
+        checkEntry1(bw.entries[0])
+
+        // Check entry 2
+        checkEntry2(bw.entries[1])
+
+        // Test the export function by exporting and
+        // reimporting the data, then re-checking the
+        // new export
+        val str = BitwardenJson.export(bw.entries)
+        println(str)
+        val result = BitwardenJson.import(str)
+
+        // Run the same checks as before on the new export
+        assertEquals(2, result.entries.size)
+        checkEntry1(result.entries[0])
+        checkEntry2(result.entries[1])
+
+    }
+
+    fun checkEntry1(token: GenericJsonEntry) {
         assertEquals("mysecret", token.secret)
         assertEquals("Login Item's Name", token.issuer)
         assertEquals("", token.name)
+    }
 
-        val token2 = bw.entries[1]
-        assertEquals("abc123", token2.secret)
-        assertEquals("Facebook", token2.issuer)
-        assertEquals("MyUsername", token2.name)
-
-        // TODO test json
-        val str = BitwardenJson.export(bw.entries)
-        val result = BitwardenJson.import(str)
-        // TODO check result
+    fun checkEntry2(token: GenericJsonEntry) {
+        assertEquals("abc123", token.secret)
+        assertEquals("Facebook", token.issuer)
+        assertEquals("MyUsername", token.name)
     }
 
 }
--- app/src/test/java/dev/mcarr/totpconverter/classes/bitwarden/BitwardenTestData.kt
+++ app/src/test/java/dev/mcarr/totpconverter/classes/bitwarden/BitwardenTestData.kt
@@ -1,47 +1,91 @@
 package dev.mcarr.totpconverter.classes.bitwarden
 
-fun BitwardenTestData(): String {
-    return """
+object BitwardenTestData {
+
+    fun getFullObject(): String {
+
+        val item1 = getLoginItem1()
+        val item2 = getLoginItem2()
+        val item3 = getCardItem1()
+        val item4 = getSecureNoteItem1()
+        val item5 = getIdentityItem1()
+
+        return """
+            { "items": [ $item1, $item2, $item3, $item4, $item5 ] }
+        """.trimIndent()
+    }
+
+    fun getLoginItem1(): String = """
         {
-          "items": [
-            {
             "type": 1,
             "name": "Login Item's Name",
-            "login": {
-              "totp": "otpauth://totp/mysecret",
-              "uris":[{
-                "match":null,
-                "uri":"https://my.domain.com"
-              }]
-            }
-          },
-          {
+            "login": ${getLogin1()}
+        }
+    """.trimIndent()
+
+    fun getLogin1(): String = """
+        {
+            "username": "testname",
+            "password": "testpass",
+            "totp": "otpauth://totp/mysecret",
+            "uris":[${getLoginUri1()}]
+        }
+    """.trimIndent()
+
+    fun getLoginUri1(): String = """
+        {
+            "match":null,
+            "uri":"https://my.domain.com"
+        }
+    """.trimIndent()
+
+    fun getSecureNoteItem1(): String = """
+        {
             "type": 2,
             "name": "Secure Note Item's Name",
             "secureNote": {}                     
-          },
-          {
-            "type": 3,
-            "name": "Card Item's Name",
-            "card": {}                         
-          },
-          {
-            "type": 4,
-            "name": "Identity Item's Name",
-            "identity": {}                     
-          },
-          {
-          "type": 1,
-          "name": "MyUsername",
-          "login": {
-            "totp": "otpauth://totp/MyUsername?issuer=Facebook&secret=abc123",
-            "uris":[{
-              "match":null,
-              "uri":"https://my.domain.com"
-            }]
-          }
-        }
-          ]
         }
     """.trimIndent()
+
+    fun getCardItem1(): String = """
+        {
+            "type": 3,
+            "name": "Card Item's Name",
+            "card": ${getCard1()}              
+        }
+    """.trimIndent()
+
+    fun getCard1(): String = """
+        {
+            "cardholderName": "Mr Test",
+            "brand": "Mastercard",
+            "number": "0123456789",
+            "expMonth": "1",
+            "expYear": "1990",
+            "code": "456"
+        }
+    """.trimIndent()
+
+    fun getIdentityItem1(): String = """
+        {
+            "type": 4,
+            "name": "Identity Item's Name",
+            "identity": {}                    
+        }
+    """.trimIndent()
+
+    fun getLoginItem2(): String = """
+        {
+            "type": 1,
+            "name": "MyUsername",
+            "login": {
+                "totp": "otpauth://totp/MyUsername?issuer=Facebook&secret=abc123",
+                "uris":[{
+                    "match":null,
+                    "uri":"https://my.domain.com"
+                }]
+            }
+        }
+    """.trimIndent()
+
 }
