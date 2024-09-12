8deb294abd3859c2a178fc7ccf8a27862ea0ad5b
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-09-12 17:18:17 +1000

    Added enum for defining navigation endpoints

---
+++ app/src/main/java/dev/mcarr/totpconverter/enums/Destination.kt
@@ -0,0 +1,20 @@
+package dev.mcarr.totpconverter.enums
+
+/**
+ * Object which contains String keys for each individual
+ * composable route in the app.
+ *
+ * These values are used to identify individual screens
+ * or components, and to navigate between them from the
+ * NavGraph in MainActivity.
+ *
+ * @see dev.mcarr.totpconverter.MainActivity
+ * */
+object Destination{
+    const val INPUT_FORMAT = "INPUT_FORMAT"
+    const val INPUT_FILE = "INPUT_FILE"
+    const val INPUT_PROCESSING = "INPUT_PROCESSING"
+    const val OUTPUT_FORMAT = "OUTPUT_FORMAT"
+    const val OUTPUT_PROCESSING = "OUTPUT_PROCESSING"
+    const val RESULT = "RESULT"
+}
