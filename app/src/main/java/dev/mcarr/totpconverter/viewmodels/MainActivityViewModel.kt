7da159085d108d8b70305992f6ef6af3fbf71ae4
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-09-12 17:17:46 +1000

    Added viewmodel for main activity navigation

---
+++ app/src/main/java/dev/mcarr/totpconverter/viewmodels/MainActivityViewModel.kt
@@ -0,0 +1,35 @@
+package dev.mcarr.totpconverter.viewmodels
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dev.mcarr.totpconverter.enums.Destination
+
+/**
+ * Viewmodel for MainActivity.
+ *
+ * @see dev.mcarr.totpconverter.MainActivity
+ * @see dev.mcarr.totpconverter.ui.screens.MainActivityScreen
+ * */
+class MainActivityViewModel : ViewModel() {
+
+    /**
+     * The current route.
+     *
+     * Used to keep track of which screen we're currently
+     * viewing, for the sake of altering other parts of
+     * the UI.
+     *
+     * eg. To hide the "back" navigation, or to show
+     * different actions in the action bar.
+     * */
+    var route = mutableStateOf("")
+
+    /**
+     * Start destination for the NavGraph.
+     *
+     * This value should ONLY be changed for unit testing
+     * or preview purposes.
+     * */
+    var startDestination = Destination.INPUT_FORMAT
+
+}
